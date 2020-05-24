064a6294452523d5b926c021388aefc7ef5e3088
virginiah894 <virgyperry@gmail.com>
2020-05-24 09:54:20 +0300

    file

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,10 @@
 from __future__ import unicode_literals
 from django.contrib import admin
- from .models import Image,Location,Category
+from .models import Image,Location,Category
 
+
+
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
 # Register your models here.
