1d9d9a1bbb0638418d7998e38ae202b4be941264
virginiah894 <virgyperry@gmail.com>
2020-05-24 07:16:39 +0300

    Search template

--- gallery/views.py
+++ gallery/views.py
@@ -40,5 +40,12 @@ def search_results(request):
         message = "You haven't searched for any term"
         return render(request, 'all-images/search.html',{"message":message})
 
+def image(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-images/onepic.html", {"image":image})
+
 
 # Create your views here.
